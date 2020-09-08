97d5ad3b7c06451023889b9dfbe64ecaff8a0677
littonishir <littonishir@gmail.com>
2020-09-08 15:46:06 +0800

    增加dataBinding 基础用法

--- app/src/main/java/com/litton/jetpack0813/livedata/MyLiveDataActivity.kt
+++ app/src/main/java/com/litton/jetpack0813/livedata/MyLiveDataActivity.kt
@@ -1,29 +1,23 @@
 package com.litton.jetpack0813.livedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.litton.jetpack0813.R
-import kotlinx.android.synthetic.main.activity_my_livedata.*
+import com.litton.jetpack0813.databinding.ActivityMyLivedataBinding
 
 class MyLiveDataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_livedata)
+        val binding = DataBindingUtil.setContentView<ActivityMyLivedataBinding>(
+            this,
+            R.layout.activity_my_livedata
+        )
+
         val viewModel = ViewModelProviders.of(this).get(LiveDataViewModel::class.java)
-        viewModel.likeNum.observe(this, Observer {
-            textView.text = it.toString()
-
-        })
-
-        imageButton1.setOnClickListener {
-            viewModel.add(1)
-        }
-        imageButton2.setOnClickListener {
-            viewModel.add(-1)
-        }
+        binding.data = viewModel
+        binding.lifecycleOwner = this
     }
 }
